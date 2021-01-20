bf5a2bc433bfd09c66583d8ea0b9801ac5c68b5e
federicadomani <federicadomani@mailfence.com>
2021-01-20 02:54:57 -0800

    Version 5.9.3.1: minor bug fixes

--- AutoClicker2/Properties/AssemblyInfo.cs
+++ AutoClicker2/Properties/AssemblyInfo.cs
@@ -33,6 +33,6 @@ using System.Resources;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("5.9.3.0")]
-[assembly: AssemblyFileVersion("5.9.3.0")]
+[assembly: AssemblyVersion("5.9.3.1")]
+[assembly: AssemblyFileVersion("5.9.3.1")]
 [assembly: NeutralResourcesLanguageAttribute("en")]
